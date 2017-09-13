0d9a32937ee5e357821f9a8250c6fd6b5c64fef2
Clara Bertaut <bertautc17@grinnell.edu>
2017-09-13 02:03:33 +0000

    have no idea what I'm doing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -41,5 +41,9 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+  
+  def sort
+    @movie[] = Movie.sort
+  end
 
 end
